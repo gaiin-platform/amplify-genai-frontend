09d5f50655e2e568cb548a3ee7f2f50c5684ff77
Jules White <jules.white@gmail.com>
2023-09-19 16:30:59 -0500

    Vercel resolved

--- next.config.js
+++ next.config.js
@@ -15,15 +15,5 @@ const nextConfig = {
   },
 };
 
-if (
-    process.env.LD_LIBRARY_PATH == null ||
-    !process.env.LD_LIBRARY_PATH.includes(
-        `${process.env.PWD}/node_modules/canvas/build/Release:`,
-    )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-      process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
 
 module.exports = nextConfig;
