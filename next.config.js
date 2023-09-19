c4fc8fe5ea8d894a4b00fe4877b6305e48db4030
Jules White <jules.white@gmail.com>
2023-09-19 16:45:36 -0500

    Removed PDF text extraction for Vercel

--- next.config.js
+++ next.config.js
@@ -15,15 +15,15 @@ const nextConfig = {
   },
 };
 
-if (
-    process.env.LD_LIBRARY_PATH == null ||
-    !process.env.LD_LIBRARY_PATH.includes(
-        `${process.env.PWD}/node_modules/canvas/build/Release:`,
-    )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-      process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
+// if (
+//     process.env.LD_LIBRARY_PATH == null ||
+//     !process.env.LD_LIBRARY_PATH.includes(
+//         `${process.env.PWD}/node_modules/canvas/build/Release:`,
+//     )
+// ) {
+//   process.env.LD_LIBRARY_PATH = `${
+//       process.env.PWD
+//   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+// }
 
 module.exports = nextConfig;
