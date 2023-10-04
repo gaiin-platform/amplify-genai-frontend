d120a0f29ab0b149bfcc63031b060fb8bdca962f
Jules White <jules.white@gmail.com>
2023-10-04 08:19:15 -0500

    Added Auth0 auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,27 @@
-import { Toaster } from 'react-hot-toast';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-import { appWithTranslation } from 'next-i18next';
-import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
+import {Toaster} from 'react-hot-toast';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import {appWithTranslation} from 'next-i18next';
+import type {AppProps} from 'next/app';
+import {Inter} from 'next/font/google';
 
 import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({subsets: ['latin']});
 
-function App({ Component, pageProps }: AppProps<{}>) {
-  const queryClient = new QueryClient();
+function App({ Component, pageProps }: AppProps) {
+    const queryClient = new QueryClient();
 
-  return (
-    <div className={inter.className}>
-      <Toaster />
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </div>
-  );
+    return (
+        <UserProvider>
+            <div className={inter.className}>
+                <Toaster/>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                </QueryClientProvider>
+            </div>
+        </UserProvider>
+    );
 }
 
 export default appWithTranslation(App);
