a967a8cf89d478c93acca0f1a0e8716c5f61f4b4
Karns, Allen B <allen.karns@vanderbilt.edu>
2023-11-20 13:41:26 -0600

    Update Tailwind to Contect from Purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: [
+  content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
