946c226472a2e561547d62c2f6a6b3c0f925e4b9
Karns, Allen B <allen.karns@vanderbilt.edu>
2023-11-20 13:41:41 -0600

    Add logging for debug

--- utils/app/api.ts
+++ utils/app/api.ts
@@ -1,5 +1,13 @@
 import { Plugin, PluginID } from '@/types/plugin';
 
+console.log(process.env.AUTH0_CLIENT_ID);
+console.log(process.env.AUTH0_CLIENT_SECRET);
+console.log(process.env.AUTH0_SCOPE);
+console.log(process.env.AUTH0_AUDIENCE);
+console.log(process.env.AUTH0_SECRET);
+export const AUTH0_SECRET=process.env.AUTH0_SECRET;
+console.log(AUTH0_SECRET);
+
 export const getEndpoint = (plugin: Plugin | null) => {
   if (!plugin) {
     return 'api/chat';
