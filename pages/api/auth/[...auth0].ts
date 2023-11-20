afa81a75e75d2231074fa011603d828f52c198b6
Karns, Allen B <allen.karns@vanderbilt.edu>
2023-11-20 15:59:02 -0600

    remove console log from handle auth

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,15 +1,11 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-
-console.log(process.env.AUTH0_CLIENT_ID);
-console.log(process.env.AUTH0_CLIENT_SECRET);
-console.log(process.env.AUTH0_SCOPE);
-console.log(process.env.AUTH0_AUDIENCE);
-console.log(process.env.AUTH0_SECRET);
+//Only Needed for Amplify 
 export const AUTH0_SECRET=process.env.AUTH0_SECRET;
 export const issuerBaseURL=process.env.AUTH0_ISSUER_BASE_URL;
 export const baseURL=process.env.AUTH0_BASE_URL;
 export const clientID=process.env.AUTH0_CLIENT_ID;
 export const clientSecret=process.env.AUTH0_CLIENT_SECRET;
+
 console.log(AUTH0_SECRET);
 
 export default handleAuth({
