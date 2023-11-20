68163530f6f1d527e44276ceb1c16b224576eb06
Karns, Allen B <allen.karns@vanderbilt.edu>
2023-11-20 14:00:25 -0600

    move var export to auth0 module

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,5 +1,12 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
+console.log(process.env.AUTH0_CLIENT_ID);
+console.log(process.env.AUTH0_CLIENT_SECRET);
+console.log(process.env.AUTH0_SCOPE);
+console.log(process.env.AUTH0_AUDIENCE);
+console.log(process.env.AUTH0_SECRET);
+export const AUTH0_SECRET=process.env.AUTH0_SECRET;
+console.log(AUTH0_SECRET);
 
 export default handleAuth({
     // @ts-ignore
