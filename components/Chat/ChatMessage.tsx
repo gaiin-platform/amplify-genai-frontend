d9b927fe1a9f1ac9d947cd56a60465fb39d1af6e
Jules White <jules.white@gmail.com>
2024-03-22 04:33:09 -0500

    Moved data source display in messages to separate component

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -33,6 +33,7 @@ import {LoadingDialog} from "@/components/Loader/LoadingDialog";
 import StatusDisplay from "@/components/Chatbar/components/StatusDisplay";
 import PromptingStatusDisplay from "@/components/Status/PromptingStatusDisplay";
 import ChatSourceBlock from "@/components/Chat/ChatContentBlocks/ChatSourcesBlock";
+import DataSourcesBlock from "@/components/Chat/ChatContentBlocks/DataSourcesBlock";
 
 
 export interface Props {
@@ -244,45 +245,7 @@ export const ChatMessage: FC<Props> = memo(({
                                         <div className="prose whitespace-pre-wrap dark:prose-invert flex-1">
                                             {message.label || message.content}
                                         </div>
-                                        {message.data && message.data.dataSources && message.data.dataSources.length > 0 && (
-                                            <div className="flex flex-col w-full mt-5 text-gray-800">
-                                                <div className="mr-3 dark:text-white">Included documents:</div>
-
-                                                <div className="flex flex-col">
-                                                    {message.data && message.data.dataSources && message.data.dataSources.map((d: any, i: any) => (
-                                                        <div
-                                                            key={i}
-                                                            className="bg-yellow-400 dark:bg-[#B0BEC5] rounded-xl shadow-lg h-12 mr-2 mb-2"
-                                                        >
-                                                            <div className="flex flex-row">
-                                                                <div
-                                                                    className="w-14 h-12 flex-none bg-cover rounded-l-xl text-center overflow-hidden"
-                                                                    style={{backgroundImage: 'url("/sparc_apple.png")'}}
-                                                                    title={d.name}>
-                                                                </div>
-                                                                <div className="ml-3 mt-3">
-                                                                    <IconFileCheck/>
-                                                                </div>
-                                                                <div className="mt-3 ml-1 flex-grow p-0 truncate">
-                                                                    {i + 1}. {d.name}
-                                                                </div>
-                                                                {d.id && d.id.startsWith("s3://") && (
-                                                                    <div className="mt-3 mr-3 ml-1 p-0 truncate"
-                                                                    >
-                                                                        <button onClick={() => {
-                                                                            handleDownload(d);
-                                                                        }}
-                                                                        >
-                                                                            <IconDownload/>
-                                                                        </button>
-                                                                    </div>
-                                                                )}
-                                                            </div>
-                                                        </div>
-                                                    ))}
-                                                </div>
-                                            </div>
-                                        )}
+                                        <DataSourcesBlock message={message} handleDownload={handleDownload}/>
                                     </div>
                                     <div className="flex flex-row">
                                         {(isEditing || messageIsStreaming) ? null : (
