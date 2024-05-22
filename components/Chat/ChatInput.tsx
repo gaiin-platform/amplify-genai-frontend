3e8afe8a38fdfb5c5123fd758455ae81e2eee746
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2024-05-22 16:34:06 -0500

    qisummary button clash with 'stop generating' button fix

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -526,11 +526,11 @@ const onAssistantChange = (assistant: Assistant) => {
             <div
                 className="flex flex-col justify-center items-center stretch mx-2 mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:mt-[52px] md:last:mb-6 lg:mx-auto lg:max-w-3xl">
                
-               {!showScrollDownButton && !showDataSourceSelector && !showAssistantSelect && 
-               (selectedConversation && selectedConversation.messages.length > 0) && featureFlags.inCognitoGroup && (
+               {!showScrollDownButton && !messageIsStreaming && featureFlags.inCognitoGroup &&
+               (selectedConversation && selectedConversation.messages.length > 0) &&  (
                <div className="fixed flex flex-row absolute top-0 group prose dark:prose-invert">
                 <button
-                    className="mt-5 cursor-pointer border border-gray-300 dark:border-gray-600 rounded px-2 py-1"
+                    className="mt-4 cursor-pointer border border-gray-300 dark:border-gray-600 rounded px-2 py-1"
                     style={{ fontSize: '0.9rem' }} 
                     onClick={async () => {
                         setQiIsLoading(true);
