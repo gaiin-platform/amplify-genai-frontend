17b2fe8a192f3b44d98f0138cd8ce9a2fc575341
maxmoundas <maximillian.r.moundas@vanderbilt.edu>
2024-09-23 12:58:51 -0500

    show public assistantId to user

--- components/Promptbar/components/AssistantModal.tsx
+++ components/Promptbar/components/AssistantModal.tsx
@@ -571,7 +571,7 @@ export const AssistantModal: FC<Props> = ({assistant, onCancel, onSave, onUpdate
                                     </div>
                                     <input
                                         className="mt-2 w-full rounded-lg border border-neutral-500 px-4 py-2 text-neutral-900 shadow focus:outline-none dark:border-neutral-800 dark:border-opacity-50 dark:bg-[#40414F] dark:text-neutral-100"
-                                        value={definition.id || assistant.id || ""}
+                                        value={definition.assistantId || ""}
                                         disabled={true}
                                     />
 
