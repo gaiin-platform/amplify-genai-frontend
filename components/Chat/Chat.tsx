2c81b00342be9045a26d08eb965d52d087c03166
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2024-09-24 15:00:33 -0500

    chatinput resize listeners,  ds link bug

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -213,7 +213,7 @@ export const Chat = memo(({stopConversationRef}: Props) => {
                                         });
             }
             if (selectedAssistant?.definition.name === "Standard Conversation" && selectedConversation?.model?.id) setSelectedModelId(selectedConversation?.model?.id as OpenAIModelID);
-        }, [selectedAssistant]);
+        }, [selectedAssistant, selectedConversation]);
 
         const updateMessage = (selectedConversation: Conversation, updatedMessage: Message, updateIndex: number) => {
             let updatedConversation = {
@@ -350,7 +350,7 @@ export const Chat = memo(({stopConversationRef}: Props) => {
             for (const sourceType of sources as any[]) {
                 const sourceValues: any = Object.values(sourceType.sources);
                 for (const subSource of sourceValues) {
-                    if (subSource.contentKey && filename === subSource.name.replace(/&/g, ' ')) {
+                    if (subSource.contentKey && subSource.name === filename.replace(/&/g, ' ')) {
                         ds = {id: subSource.contentKey, name: subSource.name, type: subSource.type}
                         if (subSource.groupId) groupId = subSource.groupId;
                         break;
@@ -359,6 +359,8 @@ export const Chat = memo(({stopConversationRef}: Props) => {
                 if (ds) {
                     await downloadDataSourceFile(ds, groupId);
                     break; 
+                } else {
+                    console.log("No content key found")
                 }
             }
 
@@ -373,8 +375,8 @@ export const Chat = memo(({stopConversationRef}: Props) => {
             if (selectedConversation) {
                 let [category, action_path] = href.slice(1).split(":");
 
-                switch (category) {
-                    case ("chat"):
+                switch (true) {
+                    case (category === "chat"):
                         let [action, path] = action_path.split("/");
                         if (action === "send") {
                             const content = path;
@@ -389,7 +391,7 @@ export const Chat = memo(({stopConversationRef}: Props) => {
                             }
                         }
                         break;
-                    case ("dataSource"):
+                    case (['dataSources', 'dataSource'].includes(category)):
                         handleDownloadFile(message, action_path);
                         break;
                     default:
