c7aa34dbb211813128c8bec397b1c8835a1a6c62
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2024-09-25 14:07:06 -0500

    ds link bug fix

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -353,7 +353,7 @@ export const Chat = memo(({stopConversationRef}: Props) => {
             for (const sourceType of sources as any[]) {
                 const sourceValues: any = Object.values(sourceType.sources);
                 for (const subSource of sourceValues) {
-                    if (subSource.contentKey && subSource.name === filename.replace(/&/g, ' ')) {
+                    if (subSource.contentKey && !subSource.contentKey.includes("global/") && subSource.name === filename.replace(/&/g, ' ')) {
                         ds = {id: subSource.contentKey, name: subSource.name, type: subSource.type}
                         if (subSource.groupId) groupId = subSource.groupId;
                         break;
@@ -364,6 +364,7 @@ export const Chat = memo(({stopConversationRef}: Props) => {
                     break; 
                 } else {
                     console.log("No content key found")
+                    alert("Unable to provide the document at this time.");
                 }
             }
 
