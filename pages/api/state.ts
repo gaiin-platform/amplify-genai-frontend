gaiin-platform/amplify-genai-frontend

commit c2973c8380c8ec0d9e48db8a5a6556dcf5ca4983

update apis to base_api_url

pages/api/state.ts
@@ -22,7 +22,7 @@ const saveState =
 
         const { accessToken } = session;
 
-        const apiUrl = process.env.STATE_API_URL || ""; // API Gateway URL from environment variables
+        const apiUrl = process.env.API_BASE_URL + "/state" || ""; // API Gateway URL from environment variables
 
         // Accessing itemData parameters from the request
         const itemData = req.body;
@@ -61,7 +61,7 @@ export const getState =
 
         const { accessToken } = session;
 
-        const apiUrl = process.env.STATE_API_URL || ""; // API Gateway URL from environment variables
+        const apiUrl = process.env.API__BASE_URL + "state" || ""; // API Gateway URL from environment variables
 
         // Accessing itemData parameters from the request
         const stateId = req.query.id;

pages/api/accounts/op.ts
@@ -14,7 +14,7 @@ const accountOp =
 
         const { accessToken } = session;
 
-        let apiUrl = process.env.STATE_API_URL || "";
+        let apiUrl = process.env.API_BASE_URL + "/state" || "";
 
         // Accessing itemData parameters from the request
         const reqData = req.body;

pages/api/assistant/op.ts
@@ -12,7 +12,7 @@ const assistantOp =
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
-        let apiUrl = process.env.ASSISTANT_API_URL || "";
+        let apiUrl = process.env.API_BASE_URL + "/assistant" || "";
 
         // Accessing itemData parameters from the request
         const reqData = req.body;

pages/api/baseprompts/op.ts
@@ -14,7 +14,7 @@ const basePromptOp =
 
         const { accessToken } = session;
 
-        let apiUrl = process.env.STATE_API_URL || "";
+        let apiUrl = process.env.API_BASE_URL + "/state" || "";
 
         // Accessing itemData parameters from the request
         const reqData = req.body;

pages/api/download/op.ts
@@ -15,7 +15,7 @@ const downloadOp =
 
         const { accessToken } = session;
 
-        let apiUrl = process.env.CONVERT_API_URL || "";
+        let apiUrl = process.env.API_BASE_URL + "/chat" || "";
 
         // Accessing itemData parameters from the request
         const reqData = req.body;

pages/api/files/upload.ts
@@ -16,7 +16,7 @@ const getPresignedUrl =
         const { accessToken } = session;
 
         const itemData = req.body;
-        const apiUrl = (process.env.FILES_API_URL || "") + '/upload'; // API Gateway URL from environment variables
+        const apiUrl = (process.env.API_BASE_URL + "/assistant/files" || "") + '/upload'; // API Gateway URL from environment variables
 
         try {
 

pages/api/market/op.ts
@@ -15,7 +15,7 @@ const marketOp =
 
         const { accessToken } = session;
 
-        let apiUrl = process.env.MARKET_API_URL || "";
+        let apiUrl = process.env.API_BASE_URL + "/market" || "";
 
         // Accessing itemData parameters from the request
         const reqData = req.body;

pages/api/share.ts
@@ -21,7 +21,7 @@ const shareExport =
 
         const { accessToken } = session;
 
-        const apiUrl = process.env.SHARE_API_URL || ""; // API Gateway URL from environment variables
+        const apiUrl = process.env.API_BASE_URL + "/state/share" || ""; // API Gateway URL from environment variables
 
         // Accessing itemData parameters from the request
         const itemData = req.body;

commit 91a005ba757c6a0b1ceb848d899e4b2cb0252285

Fix typo
c2973c8380c8ec0d9e48db8a5a6556dcf5ca4983 introduced a small typo while
refactoring.

pages/api/state.ts
@@ -61,7 +61,7 @@ export const getState =
 
         const { accessToken } = session;
 
-        const apiUrl = process.env.API__BASE_URL + "state" || ""; // API Gateway URL from environment variables
+        const apiUrl = process.env.API_BASE_URL + "state" || ""; // API Gateway URL from environment variables
 
         // Accessing itemData parameters from the request
         const stateId = req.query.id;