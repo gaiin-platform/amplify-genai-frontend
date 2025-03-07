3472bd71f717b1b76e6bd204993fc8acfa68315d
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2025-03-06 23:29:41 -0600

    Integration Support + Refactor

--- services/oauthIntegrationsService.ts
+++ services/oauthIntegrationsService.ts
@@ -1,7 +1,8 @@
-
+import { IntegrationProviders, IntegrationSecretsMap, IntegrationsMap } from "@/types/integrations";
 import { doRequestOp } from "./doRequestOp";
 
-const URL_PATH = '/integrations/oauth';
+const URL_PATH = '/integrations'; 
+const OAUTH_PATH = '/oauth'; 
 const SERVICE_NAME = "oauth";
 
 // This takes the name of the integration, such as "google_sheets" which will need a corresponding
@@ -9,7 +10,7 @@ const SERVICE_NAME = "oauth";
 export const getOauthRedirect = async (integration: string) => {
     const op = {
         method: 'POST',
-        path: URL_PATH,
+        path: URL_PATH + OAUTH_PATH,
         op: "/start-auth",
         data: { integration },
         service: SERVICE_NAME
@@ -17,12 +18,11 @@ export const getOauthRedirect = async (integration: string) => {
     return await doRequestOp(op);
 }
 
-export const getUserIntegrations = async (integrations: string[]) => {
+export const getConnectedIntegrations = async () => {
     const op = {
-        method: 'POST',
-        path: URL_PATH,
+        method: 'GET',
+        path: URL_PATH + OAUTH_PATH,
         op: "/user/list",
-        data: { integrations },
         service: SERVICE_NAME
     };
     return await doRequestOp(op);
@@ -31,7 +31,7 @@ export const getUserIntegrations = async (integrations: string[]) => {
 export const deleteUserIntegration = async (integration: string) => {
     const op = {
         method: 'POST',
-        path: URL_PATH,
+        path: URL_PATH + OAUTH_PATH,
         op: "/user/delete",
         data: { integration },
         service: SERVICE_NAME
@@ -41,5 +41,81 @@ export const deleteUserIntegration = async (integration: string) => {
         console.log("Error occured with Delete User Integrations: ", response)
         alert("An error occurred. Please try again.");
     }
+    return response.success;
 
 }
+
+
+export const checkActiveIntegrations = async (integrations: string[]) => {
+
+    const integrationSecrets: IntegrationSecretsMap = {};
+    const availableIntegrations: IntegrationsMap = {};
+  
+    for (const i of integrations) {
+      const op = {
+        method: 'GET',
+        path: "",
+        op: `/${i}/integrations`,
+        service: SERVICE_NAME
+      };
+      const response = await doRequestOp(op);
+      if (response.success && response.data) {
+        const data = response.data;
+        availableIntegrations[i as IntegrationProviders] = data.integrations;
+        if (data.secrets) integrationSecrets[i as IntegrationProviders] = data.secrets;
+      }
+    }
+  
+    return { integrationLists: availableIntegrations, secrets: integrationSecrets};
+};
+
+
+export const getAvailableIntegrations = async () => {
+    const op = {
+        method: 'GET',
+        path: URL_PATH,
+        op: "/list_supported",
+        service: SERVICE_NAME
+    };
+    return await doRequestOp(op);
+}
+
+
+export const registerIntegrationSecrets = async (integrationSecrets: any) => {
+    const op = {
+        method: 'POST',
+        path: URL_PATH  + OAUTH_PATH,
+        op: "/register_secret",
+        data: integrationSecrets,
+        service: SERVICE_NAME
+    };
+    return await doRequestOp(op);
+}
+
+
+
+export const listIntegrationFiles = async (data: any) => {
+    const op = {
+        method: 'POST',
+        path: URL_PATH,
+        op: "/user/files",
+        data: data,
+        service: SERVICE_NAME
+    };
+    return await doRequestOp(op);
+}
+
+
+
+export const downloadIntegrationFile = async (data:any) => {
+    const op = {
+        method: 'POST',
+        path: URL_PATH,
+        op: "/user/files/download",
+        data: data,
+        service: SERVICE_NAME
+    };
+    return await doRequestOp(op);
+}
+
+
