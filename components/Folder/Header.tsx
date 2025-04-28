7c3e5e50b14f6f239490813b1b95bc9562eafb25
Alexander Li <alli@davidson.edu>
2025-04-28 15:11:48 -0400

    Changed header and lowered open/close buttons

--- components/Folder/Header.tsx
+++ components/Folder/Header.tsx
@@ -1,12 +1,13 @@
 export default function Header(){
     return (
-        <header className="bg-red-#D42121 text-black py-4 sticky top-0 z-50">
+        <header className="bg-red text-black py-4 sticky top-0 z-50">
             <div className="container mx-auto px-4 flex justify-between items-center">
-                <div className="logo">
-                </div>
                 <div className="headerText">
                     <h1 className="text-xl font-semibold">Amplify</h1> 
                 </div> 
+                <div className="logo">
+                    <img className="DClogo" src="davidsonwordmark-lockup-on red.png"></img>
+                </div>
             </div>
         </header>
     );
--- components/Sidebar/components/OpenCloseButton.tsx
+++ components/Sidebar/components/OpenCloseButton.tsx
@@ -11,9 +11,9 @@ export const CloseSidebarButton = ({ onClick, side, isDisabled}: Props) => {
   return (
     <>
       <button
-        className={`fixed top-5 ${
+        className={`fixed top-50 ${
           side === 'right' ? 'right-[280px]' : 'left-[280px]'
-        } z-50 h-7 w-7 hover:text-gray-400 dark:text-neutral-200 dark:hover:text-gray-300 sm:top-0.5 sm:${
+        } z-50 h-7 w-7 hover:text-gray-400 dark:text-neutral-200 dark:hover:text-gray-300 sm:top-25 sm:${
           side === 'right' ? 'right-[280px]' : 'left-[280px]'
         } sm:h-8 sm:w-8 sm:text-neutral-700`}
         onClick={onClick}
@@ -32,9 +32,9 @@ export const CloseSidebarButton = ({ onClick, side, isDisabled}: Props) => {
 export const OpenSidebarButton = ({ onClick, side, isDisabled }: Props) => {
   return (
     <button
-      className={`fixed top-2.5 ${
+      className={`fixed top-50 ${
         side === 'right' ? 'right-2' : 'left-2'
-      } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-neutral-200 dark:hover:text-gray-300 sm:top-0.5 sm:${
+      } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-neutral-200 dark:hover:text-gray-300 sm:top-25 sm:${
         side === 'right' ? 'right-2' : 'left-2'
       } sm:h-8 sm:w-8 sm:text-neutral-700`}
       onClick={onClick}
