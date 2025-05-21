267b89a25d394d47841d0076e638140c29c103b9
Jules White <juleswhite@Juless-MacBook-Pro-7.local>
2025-05-21 05:38:13 -0500

    Updated ui

--- components/Chat/Avatars.tsx
+++ components/Chat/Avatars.tsx
@@ -25,7 +25,9 @@ export const Amplify: React.FC = () => {
                 <div className="avatar-inner-circle avatar-inner-assistant">
                     <div className="amplify-icon">
                         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2L15.09 8.26L22 9L17 14.74L18.18 22.26L12 18.77L5.82 22.26L7 14.74L2 9L8.91 8.26L12 2Z" fill="currentColor"/>
+                            <path d="M12 2L9.5 8.5L3 9L8 14L7 21L12 18.5L17 21L16 14L21 9L14.5 8.5L12 2Z" fill="currentColor"/>
+                            <path d="M7.5 3.5L6.5 6L4 6.5L6 8.5L5.5 11L7.5 9.5L9.5 11L9 8.5L11 6.5L8.5 6L7.5 3.5Z" fill="currentColor" opacity="0.7"/>
+                            <path d="M17.5 4L16.8 5.8L15 6.5L16.8 7.2L17.5 9L18.2 7.2L20 6.5L18.2 5.8L17.5 4Z" fill="currentColor" opacity="0.5"/>
                         </svg>
                     </div>
                 </div>
