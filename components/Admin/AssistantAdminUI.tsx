a8bad0ea13105a37e13970b6a51f34ab102b1157
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2025-08-04 10:54:05 -0500

    Build Fix - formatting issue in AssistantAdminUI component by correcting indentation for groupConvAnalysisSupport prop

--- components/Admin/AssistantAdminUI.tsx
+++ components/Admin/AssistantAdminUI.tsx
@@ -551,7 +551,8 @@ export const AssistantAdminUI: FC<Props> = ({ open, openToGroup, openToAssistant
                             groupTypes={selectedGroup?.groupTypes}
                             additionalGroupData={additionalGroupData}
                             setAdditionalGroupData={setAdditionalGroupData}
-                            groupConvAnalysisSupport={!!selectedGroup?.supportConvAnalysis
+                            groupConvAnalysisSupport={!!selectedGroup?.supportConvAnalysis}
+
                         />
                     </AssistantModal>
                 </div>
