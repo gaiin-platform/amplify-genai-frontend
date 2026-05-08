c5e10ab35df8212c7600e28499dca14a8d30b751
hornepm <prestonmhorne@icloud.com>
2026-05-08 10:55:14 -0500

    fix: redundant nav bar appearing on smaller windows

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -42,7 +42,6 @@ import { Prompt } from '@/types/prompt';
 
 import { Chat } from '@/components/Chat/Chat';
 import { Chatbar } from '@/components/Chatbar/Chatbar';
-import { Navbar } from '@/components/Mobile/Navbar';
 import Promptbar from '@/components/Promptbar';
 import {
     Icon3dCubeSphere,
@@ -1567,14 +1566,7 @@ const Home = ({
                     <main
                         className={`flex h-screen w-screen flex-col text-sm text-white dark:text-white ${lightMode}`}
                     >
-                        <div className="fixed top-0 w-full sm:hidden">
-                            <Navbar
-                                selectedConversation={selectedConversation}
-                                onNewConversation={handleNewConversation}
-                            />
-                        </div>
-
-                        <div className="flex h-full w-full pt-[48px] sm:pt-0">
+                        <div className="flex h-full w-full">
                             <UserMenu
                                 email={user?.email}
                                 name={session?.user?.name}
